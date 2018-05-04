a8ea3562f858284009e3a2e3eee488af13902cf9
saadq <saad@saadq.com>
2018-05-03 21:10:18 -0400

    remove tslint shadowed variable

--- packages/lynt/src/tslint/index.ts
+++ packages/lynt/src/tslint/index.ts
@@ -23,27 +23,27 @@ function tslint(paths: Array<string>, options: LyntOptions): LyntResults {
     formatter: options.json ? 'json' : 'stylish'
   }
 
-  let tslint: Linter
+  let linter: Linter
   let filesToLint: Array<string>
 
   if (projectRoot && paths.length === 0) {
     const tsconfig = join(projectRoot, 'tsconfig.json')
     const program = Linter.createProgram(tsconfig, projectRoot)
-    tslint = new Linter(tslintOptions, program)
+    linter = new Linter(tslintOptions, program)
     filesToLint = Linter.getFileNames(program)
   } else {
-    tslint = new Linter(tslintOptions)
+    linter = new Linter(tslintOptions)
     filesToLint = globby.sync(paths)
   }
 
   filesToLint.forEach(file => {
     const fileContents = readFileSync(file, 'utf8')
-    tslint.lint(file, fileContents, tslintConfig)
+    linter.lint(file, fileContents, tslintConfig)
   })
 
   unlinkSync(configPath)
 
-  const { errorCount, output } = tslint.getResult()
+  const { errorCount, output } = linter.getResult()
 
   const results: LyntResults = {
     errorCount,
