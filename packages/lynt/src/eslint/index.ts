b233701be4e6395039121f89b3f00a5bffeada7f
saadq <saad@saadq.com>
2018-05-03 21:03:56 -0400

    go back to simple function API and add basic tests

--- packages/lynt/src/eslint/index.ts
+++ packages/lynt/src/eslint/index.ts
@@ -2,58 +2,23 @@ import { CLIEngine } from 'eslint'
 import getConfig from './config'
 import { LyntOptions, LyntResults } from '../types'
 
-class ESLint {
-  eslint: CLIEngine
-  formatter: CLIEngine.Formatter
-  fix: boolean
+function eslint(paths: Array<string>, options: LyntOptions): LyntResults {
+  const config = getConfig(options)
+  const engine = new CLIEngine(config)
+  const report = engine.executeOnFiles(paths)
+  const format = options.json ? 'json' : 'stylish'
+  const formatter = engine.getFormatter(format)
 
-  /**
-   * Create a new ESLint instance.
-   *
-   * @param options A config object that lets you customize how lynt works.
-   */
-  constructor(options: LyntOptions) {
-    const config = getConfig(options)
-    this.eslint = new CLIEngine(config)
-    this.formatter = this.eslint.getFormatter(options.json ? 'json' : 'stylish')
-    this.fix = !!options.fix
+  if (options.fix) {
+    CLIEngine.outputFixes(report)
   }
 
-  /**
-   * Lint files using ESLint.
-   *
-   * @param paths An array of glob patterns for files to be linted.
-   * @return The results of running ESLint on the files.
-   */
-  lintFiles(paths: Array<string>): LyntResults {
-    const report = this.eslint.executeOnFiles(paths)
-
-    if (this.fix) {
-      CLIEngine.outputFixes(report)
-    }
-
-    return {
-      errorCount: report.errorCount,
-      output: this.formatter(report.results)
-    }
+  const results: LyntResults = {
+    errorCount: report.errorCount,
+    output: formatter(report.results)
   }
 
-  /**
-   * Lint text using ESLint.
-   *
-   * @param text The code to be linted.
-   * @param fileName An optional file name for the given code.
-   * @return The results of running ESLint on the code.
-   */
-  lintText(text: string, fileName?: string): LyntResults {
-    const report = this.eslint.executeOnText(text, fileName)
-    const output = this.formatter(report.results)
-
-    return {
-      errorCount: report.errorCount,
-      output
-    }
-  }
+  return results
 }
 
-export default ESLint
+export default eslint
--- packages/lynt/src/index.ts
+++ packages/lynt/src/index.ts
@@ -1,41 +1,18 @@
-import { Lynter, LyntOptions, LyntResults } from './types'
+import { Lynt, LyntOptions, LyntResults } from './types'
 
-class Lynt {
-  linter: Lynter
-
-  /**
-   * Creates a Lynt instance and sets the internal linter to eslint or tslint.
-   *
-   * @param options A config object that lets you customize how lynt works.
-   */
-  constructor(options: LyntOptions = {}) {
-    const Linter: typeof Lynter = options.typescript
-      ? require('./tslint').default
-      : require('./eslint').default
-
-    this.linter = new Linter(options)
+function lynt(paths: string | Array<string>, options: LyntOptions = {}) {
+  if (!paths || (!Array.isArray(paths) && typeof paths !== 'string')) {
+    throw new TypeError('paths mnust be a string or an array of strings')
   }
 
-  /**
-   * Uses ESLint or TSLint to lint some given text.
-   *
-   * @param text The code to be linted.
-   * @param fileName An optional file name for the given code.
-   * @return The results of running ESLint or TSLint on the code.
-   */
-  lintText(text: string, fileName?: string): LyntResults {
-    return this.linter.lintText(text, fileName)
-  }
+  const lint: Lynt = options.typescript
+    ? require('./tslint').default
+    : require('./eslint').default
 
-  /**
-   * Uses ESLint or TSLint to lint a given set of files.
-   *
-   * @param paths An array of file globs that you want to lint.
-   * @return The results of running ESLint or TSLint on the files.
-   */
-  lintFiles(paths: Array<string>): LyntResults {
-    return this.linter.lintFiles(paths)
-  }
+  const files = Array.isArray(paths) ? paths : [paths]
+  const results = lint(files, options)
+
+  return results
 }
 
-export default Lynt
+export default lynt
