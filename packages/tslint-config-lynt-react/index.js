0fd4f1a2b5afd7dfe6a17e967d0b242af275f00d
saadq <saad@saadq.com>
2018-07-11 20:18:41 -0400

    simplify path to tslint-microsoft-contrib

--- packages/tslint-config-lynt-react/index.js
+++ packages/tslint-config-lynt-react/index.js
@@ -1,12 +1,9 @@
 const path = require('path')
 
-const getRuleDirectory = (name, directory) =>
-  path.join(path.dirname(require.resolve(name)), directory || '')
-
 module.exports = {
   defaultSeverity: 'error',
   extends: ['tslint-react'],
-  rulesDirectory: [getRuleDirectory('tslint-microsoft-contrib')],
+  rulesDirectory: [path.dirname(require.resolve('tslint-microsoft-contrib'))],
   rules: {
     'jsx-key': true,
     'jsx-no-string-ref': true,
